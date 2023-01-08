b583e8534b59a2c301743bec7d63c970cf06e592
Ahmed-Larbi <hamooda2002011@hotmail.com>
2023-01-08 17:04:36 +0400

    Add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ajmanGov from "../../assets/ajmangov.png"
 import ajmanMedia from "../../assets/ajmanmedia.png"
 import search from "../../assets/search.png"
@@ -6,8 +6,12 @@ import settings from "../../assets/setting.png"
 import menu from "../../assets/menu.png"
 import "./Header.css"
 import Icon from '../Icon/Icon'
+import { Sidebar, Menu, MenuItem, SubMenu, useProSidebar } from 'react-pro-sidebar';
 
 function Header() {
+  const [expanded, setExpanded] = useState(false)
+  const { collapseSidebar } = useProSidebar();
+
   return (
     <div className='header__container'>
     <div className='left__container'>
@@ -15,12 +19,25 @@ function Header() {
         <div className='vertical__line_header'></div>
         <img src={ajmanMedia} alt="" className='ajman__media'/>
     </div>
-    <div className='right__container'>
-    <Icon icon="search" size={20} alt="" className='icons' color="#797977"/>
-    <Icon icon="settings" size={20} className='icons' color="#797977"/>
-    <h1 className='arabic'> عربي </h1>
-    <Icon icon="menu"  size={20} alt="" className='icons' color="#797977"/>
-    </div>
+      <div className='right__container'>
+        <Icon icon="search" size={20} alt="" className='icons' color="#797977"/>
+        <Icon icon="settings" size={20} className='icons' color="#797977"/>
+        <h1 className='arabic'> عربي </h1>
+        <div onClick={() => collapseSidebar()}>
+
+        <Icon icon="menu"  size={20} alt="" className='icons' color="#797977"/>
+        </div>
+        </div>
+      <Sidebar style={{height: '100vh'}} collapsedWidth='0px'>
+          <Menu>
+            <SubMenu label="Charts">
+              <MenuItem> Pie charts </MenuItem>
+              <MenuItem> Line charts </MenuItem>
+            </SubMenu>
+            <MenuItem> Documentation </MenuItem>
+            <MenuItem> Calendar </MenuItem>
+          </Menu>
+        </Sidebar>
     </div>
   )
 }
